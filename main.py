5e8adfbe3d1598d1f3c8a5ece715bf6adfbfd20b
J0SIE <maksymilian.madanowski@uczen.zsk.poznan.pl>
2025-04-14 15:01:57 +0200

    Work in Progress

--- main.py
+++ main.py
@@ -17,9 +17,18 @@ class Main:
         self.output_weights2 = dh.output_weights2
 
         self.bias = dh.bias
+        self.recursion_limit = 9
+        self.recursion_counter = 1
         self.neural_network()
     def neural_network(self):
         print("Function call")
+        holder = dh.prepare_values()
+        self.weights1 = holder[0]
+        self.weights2 = holder[1]
+        self.weights3 = holder[2]
+        self.output_weights = holder[3]
+        self.output_weights2 = holder[4]
+
         # Hidden layer
         neuron1 = cm.neuron(self.inputs1, self.weights1, self.bias)
         neuron2 = cm.neuron(self.inputs2, self.weights2, self.bias)
@@ -29,7 +38,16 @@ class Main:
         output1 = cm.neuron([neuron1, neuron2, neuron3], self.output_weights, self.bias)
         output2 = cm.neuron([neuron1, neuron2, neuron3], self.output_weights2, self.bias)
 
-        print(f"Output 1: {output1}")
-        print(f"Output 2: {output2}")
+        print(f"Recursion nr {self.recursion_counter} Output 1: {output1}")
+        print(f"Recursion nr {self.recursion_counter} Output 2: {output2}")
+
+        print(f"Recursions left {self.recursion_limit}")
+
+        if(self.recursion_limit > 0):
+            self.recursion_limit -= 1
+            self.recursion_counter += 1
+            return self.neural_network()
+        else:
+            pass
 
 main = Main()
